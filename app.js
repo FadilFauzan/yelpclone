f7bf8afedd49b9be2924d66b914871889b1a3249
FadilFauzan <fadilahmadfauzan04@gmail.com>
2024-02-17 01:06:20 +0700

    membuat tampilan untuk operasi CRUD ke dalam database melalui restfull API

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const app = express()
 
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-const session = require('express-session');
+const session = require('express-session')
+const path = require('path')
 
 // define port
 const port = 3000
 
 // ejs templating
 app.set('view engine', 'ejs')
-app.set('views', 'views')
+app.set('views', path.join(__dirname, '/views'))
 
 // define middleware
 app.use(express.urlencoded({extended: true}))
@@ -33,8 +34,48 @@ mongoose.connect('mongodb://127.0.0.1:27017/bestpoint')
         console.log(error)
     }
 
-app.get('/', (req, res) =>{
-    res.send('hallo world')
+
+// define routes
+app.get('/places', async (req, res) =>{
+    const places = await Place.find()
+    res.render('places/index', {places})
+})
+
+app.get('/places/:id', async (req, res) =>{
+    const {id} = req.params
+    const place = await Place.findById(id)
+    res.render('places/show', {place})
+})
+
+app.get('/place/create', (req, res) =>{
+    res.render('places/create')
+})
+
+app.post('/places', async (req, res) =>{
+    const place = new Place(req.body)
+    // console.log(place)
+    place.save()
+    res.redirect('/places')
+})
+
+app.get('/places/:id/edit', async (req, res) =>{
+    const {id} = req.params
+    const place = await Place.findById(id)
+    res.render('places/edit', {place})
+})
+
+app.put('/places/:id', async (req, res) =>{
+    const {id} = req.params
+    await Place.findByIdAndUpdate(id, req.body,
+        {runValidators: true}
+    )
+    res.redirect(`/places/${id}`)
+})
+
+app.delete('/places/:id', async (req, res) =>{
+    const {id} = req.params
+    await Place.findByIdAndDelete(id)
+    res.redirect(`/places`)
 })
 
 app.listen(port, () =>{
