3e1372df680ca0fd40e439894d88bd9617c2cf08
FadilFauzan <fadilahmadfauzan04@gmail.com>
2024-02-20 10:33:56 +0700

    install package ejs-mate untuk layouting views,  setup bootstrap css, dan menambahkan navigasi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+const ejsMate = require('ejs-mate')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const session = require('express-session')
@@ -10,6 +11,7 @@ const path = require('path')
 const port = 3000
 
 // ejs templating
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
