8f5f32f054183eb8ed1d33e2eeee079fd051f1c9
FadilFauzan <fadilahmadfauzan04@gmail.com>
2024-03-01 21:56:57 +0700

    menambahkan middleware untuk menghapus parent data place beserta data child review

--- models/place.js
+++ models/place.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const review = require('./review')
+const Review = require('./review')
 const Schema = mongoose.Schema
 
 const placeSchema = new Schema({
@@ -12,6 +12,12 @@ const placeSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Review'
     }] 
+})
+
+placeSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({_id: {$in: doc.reviews}})
+    }
 })
 
 module.exports = mongoose.model('Place', placeSchema)
