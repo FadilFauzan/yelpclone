846c04069905e7052e171d7c4c06d92ac9dfc282
FadilFauzan <fadilahmadfauzan04@gmail.com>
2024-03-08 09:38:01 +0700

    menambahkan author untuk data review

--- routes/reviews.js
+++ routes/reviews.js
@@ -15,11 +15,15 @@ const isAuth = require('../middlewares/isAuth')
 // Routes
 router.post('/', isAuth, isValidObjectId(`/places`), validateReview, wrapAsync(async (req, res)  =>{
     const {place_id} = req.params
+
     const review = new Review(req.body.review)
+    review.author = req.user._id
+    review.save()
+
     const place = await Place.findById(place_id)
     place.reviews.push(review)
-    review.save()
     place.save()
+
     req.flash('success_msg', 'Review added successfully')
     res.redirect(`/places/${place_id}`)
 }))
