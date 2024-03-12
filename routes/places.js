a618162ec6865b15cb002454b3ce80ce4d49b88f
FadilFauzan <fadilahmadfauzan04@gmail.com>
2024-03-12 17:00:28 +0700

    mendapatkan data file dan lanjut menjalankan upload

--- routes/places.js
+++ routes/places.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const wrapAsync = require('../utils/wrapAsync')
+const upload = require('../config/multer')
 
 // Controller
 const PlaceController = require('../controllers/place')
@@ -15,7 +16,12 @@ const { isAuthorPlace } = require('../middlewares/isAuthor');
 // Routes
 router.route('/')
     .get(wrapAsync(PlaceController.index))
-    .post(Auth, validatePlace, wrapAsync(PlaceController.store))
+    // .post(Auth, validatePlace, wrapAsync(PlaceController.store))
+    .post(Auth, upload.array('image', 5), (req, res) => {
+        console.log(req.files)
+        console.log(req.body)
+        res.send('it works')
+    })
 
 router.get('/create', Auth, PlaceController.create)
 
